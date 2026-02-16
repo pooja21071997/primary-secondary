f2ea197dc65c695a4df7e4a5369ca152924f2ce9
poojaPersonal <100690066+pooja21071997@users.noreply.github.com>
2026-02-16 11:20:11 +0530

    navigation issue fixed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withInMemoryScrolling } from '@angular/router'; 
+import { routes } from './app/app.routes';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+   providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled'
+      })
+    )
+  ]
+});
